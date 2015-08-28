b9d7c41b58d6dc4d0e7ea6c3ad51d09f57354b50
Maycon <maycon.sacht@gmail.com>
2015-08-28 11:24:00 -0300

    Implementando autenticação integrada com facebook

---
+++ app/models/user.rb
@@ -0,0 +1,9 @@
+class User < ActiveRecord::Base
+	
+	def self.find_or_create_with_omniauth(auth)
+		user = self.find_or_create_by_provider_and_uid(auth.provider, auth.uid)
+		user.assign_attributes({ name: auth.info.name, email: auth.info.email, photo_url: auth.info.image, access_token: auth.credentials.token })
+		user.save!
+		user
+	end
+end
