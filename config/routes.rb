2bad8edf36bab34c8603281a690e5e048704b260
Maycon <maycon.sacht@gmail.com>
2015-08-29 13:38:13 -0300

    Adicionado rotas para home e ops

--- config/routes.rb
+++ config/routes.rb
@@ -1,5 +1,12 @@
 Rails.application.routes.draw do
+
+  root :to => 'home#index'
+  get 'ops', to: 'ops#index'
+
+  #Auth facebook routes
   match "/auth/:provider/callback" => "sessions#create", via: [:get, :post], as: :auth_callback
   match "/auth/failure" => "sessions#failure", via: [:get, :post], as: :auth_failure
   match "/logout" => "sessions#destroy", via: [:get, :post], as: :logout
+
+  
 end
